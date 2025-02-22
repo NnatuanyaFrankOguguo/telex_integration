02d8d76b98c9a12226b33174950513f797bce908
NnatuanyaFrankOguguo <nnatuanyafrank@gmail.com>
2025-02-22 01:44:38 +0100

    ok

---
+++ src/index.ts
@@ -0,0 +1,83 @@
+import express from "express";
+import cors from "cors";
+import axios from "axios";
+import {integrationConfig} from "./integration"
+import { HfInference } from "@huggingface/inference";
+import dotenv from "dotenv";
+
+dotenv.config(); // Load environment variables
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+
+// Sample Route
+app.get("/test/:text", async (req, res) => {
+    try {
+  
+      const { text } = req.params
+  
+  
+      const client = new HfInference(process.env.HUGGING_FACE_SECRETS as string);
+      
+      const output = await client.summarization({
+          model: "facebook/bart-large-cnn",
+          inputs: text,
+          provider: "hf-inference",
+      });
+      
+      res.status(200).send({
+          success: true,
+          output});
+      
+    } catch (error) {
+      console.log(error)
+      res.status(500).json({ error: "Something went wrong" });
+    }
+  });
+
+
+app.get("/integration-spec", async(req, res) => {
+    res.json(integrationConfig)
+})
+
+
+app.post("/webhook", async(req: any, res: any) => {
+
+    const { message, settings } = req.body;
+    if (!message){
+        return res.status(400).json({error : "No text Provided"})
+    }
+    console.log("body", req.body)
+
+    const client = new HfInference(process.env.HUGGING_FACE_SECRETS as string);
+    
+    const output = await client.summarization({
+        model: "facebook/bart-large-cnn",
+        inputs: message,
+        provider: "hf-inference",
+    });
+
+    const summaryText = typeof output === "string" ? output :  output.summary_text;
+
+    const messageWordCount = message.trim().split(/\s+/).length;
+    const summaryWordCount = summaryText.trim().split(/\s+/).length;
+    const messageLenString = messageWordCount.toString();
+    
+    const outputLenString = (summaryWordCount).toString();
+
+    return res.json({
+        message: `${summaryText} \n
+         Original word count: ${messageLenString}\n
+         Summarised word count: ${outputLenString}` });
+   
+})
+
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
---
+++ src/integration.ts
@@ -0,0 +1,78 @@
+export  const integrationConfig = {
+    data: {
+      date: {
+        created_at: "2025-02-20",
+        updated_at: "2025-02-20"
+      },
+      descriptions: {
+        app_description: "Writers Revise is an innovative backend integration designed to enhance student essay writing through automated revision, editing, proofreading, and word count analysis. When a student submits an essay, the tool intercepts the text and employs advanced natural language processing to identify and correct grammatical, punctuation, and stylistic errors. In addition to delivering a refined version of the essay with contextual suggestions for clarity and precision, it also automatically calculates word counts, helping students adhere to assignment guidelines and improve conciseness. This comprehensive approach not only polishes written work but also educates students on effective writing techniques, making it a valuable learning resource.",
+        app_logo: "https://iili.io/MUnHL7.md.jpg",
+        app_name: "Writers Revise: A Smart Essay Editing, Proofreading, and Word Count Integration",
+        app_url: "https://0dtd3s5d-3000.uks1.devtunnels.ms",
+        background_color: "#00000"
+      },
+      integration_category: "Monitoring & Logging",
+      integration_type: "modifier",
+      is_active: true,
+      output: [
+        {
+          "label": "output_channel_1",
+          "value": true
+        },
+        {
+          "label": "output_channel_2",
+          "value": false
+        }
+      ],
+      key_features: [
+        "Uses advanced NLP to identify and correct grammatical, punctuation, and stylistic errors, ensuring polished and error-free essays.",
+        "Provides in-depth feedback and clarity-enhancing suggestions that help students understand and improve their writing techniques.",
+        "Automatically calculates word counts to help students meet assignment guidelines, ensuring essays remain concise and within required limits."
+      ],
+      permissions: {
+        "monitoring_user": {
+          "always_online": true,
+          "display_name": "Performance Monitor"
+        }
+      },
+      settings: [
+        {
+          label: "Occupation",
+          type: "dropdown",
+          required: true,
+          default: "Student",
+          options: ["Writer", "Student"]
+        },
+        {
+          label: "Activate W",
+          type: "checkbox",
+          required: true,
+          default: "Yes"
+        },
+        {
+          label: "Summarisation Level",
+          type: "dropdown",
+          required: true,
+          default: "Low",
+          options: ["High", "Low"]
+        },
+        {
+          label: "Alert Admin",
+          type: "multi-checkbox",
+          required: true,
+          default: "Super-Admin",
+          options: ["Super-Admin", "Admin", "Manager", "Developer"]
+        }
+      ],
+      target_url: "https://0dtd3s5d-4000.uks1.devtunnels.ms/webhook",
+      endpoint: [
+        {
+            path: "/webhook",
+            method: "POST",
+            description: "Receives messages and returns AI-generated responses",
+        },
+      ],
+      author: "Nnatuanya Frank",
+      version: "1.0.0"
+    }
+  }
